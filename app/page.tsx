cc2b0f9cd66726b524ece85259cefa8f8df02a1f
NEER4J <ittsneeraj@gmail.com>
2025-04-23 11:37:29 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default async function Home() {
+  return (
+    <>
+      <Hero />
+      
+    </>
+  );
+}
